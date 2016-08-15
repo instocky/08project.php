aec95cad9f0f63778fc6f253321e866c46de1a71
instocky <instocky@gmail.com>
2016-08-15 21:50:18 +0300

    second version

--- autopublisher/config/function.php
+++ autopublisher/config/function.php
@@ -19,4 +19,21 @@ function queryDate0($key)
   return $answer;
 }
 
+// создать массив [timecode] => 'текст статьи' timecode=числу секунд с 1-1-1970
+// равномерно распределить статьи из массива по временному диапазону
+// входные данные массив со статьями
+// первая и последняя даты публикации формат год-месяц-число(2016-05-25)
+function distrArticleDate($array =0, $startdate = 0, $finishdate = 0)
+{
+  $interval = ($finishdate - $startdate) / (count($array)-1);
+  $array2[] = $startdate;
+  foreach ($array as $key => $value) {
+    $startdate += $interval;
+    array_push($array2, (int)$startdate);
+  }
+
+  var_dump($array);
+  var_dump($array2);
+  // array_combine($array2, $array);
+}
  ?>
